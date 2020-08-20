508995397e5e5ab9e7ec0b86896ebf04762f3f5f
Ivan <ivan.novikov.1990@list.ru>
2020-08-21 00:12:51 +0300

    start

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 80
+
+app.get('/', (req, res) => {
+	res.end(`
+<div>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+		</ul>
+	</nav>
+	<h1>Home page</h1>
+</div>
+		`)
+})
+
+app.get('/about', (req, res) => {
+	res.end(`
+<div>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+		</ul>
+	</nav>
+	<h1>About page</h1>
+</div>
+`)
+})
+
+app.listen(PORT, () => {
+	console.log('Server has been started...')
+})
